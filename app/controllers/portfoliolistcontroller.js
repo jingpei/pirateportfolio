665b29e0a585e0b116b90ac8c619d1d74883e692
jingpei <jingjpei@gmail.com>
2015-03-21 20:11:50 -0400

    working controllers

--- app/controllers/portfoliolistcontroller.js
+++ app/controllers/portfoliolistcontroller.js
@@ -1,11 +1,11 @@
-function(){
+(function(){
 
   function PortfolioListController( $scope ){
 
+    portfolioApp.controller("PortfolioListController", PortfolioListController);
     //initialize scope variables
     $scope.new_portfolio = {};
     $scope.add_portfolio_error = "";
-
     $scope.portfolios = [
       {name: 'vessel1309', title: 'Call of Booty', date: '2013-09-01', description: 'Arrrgh mateys! This be the finest vessel sailing these seas that yer eyes ever laid sight on. Arrrgh.', imageurl: 'images/drunkenPirate_thumb3.png' },
       {name: 'innocents1404', title: 'The Sack of the Innocents', date: '2014-04-15', description: 'This be a detail of the sacking of the innocents where I played an important role as business analyst and in implementation.', imageurl: 'images/drunkenPirate_thumb2.png' },
@@ -13,7 +13,7 @@ function(){
     ];  
 
     //set the app for this app module
-    portfolioApp.controller("PortfolioListController", PortfolioListController);
+    
 
     //error trapping/validation
     $scope.addPortfolio = function( new_portfolio ){
@@ -23,7 +23,7 @@ function(){
       else if(!new_portfolio.title){
         $scope.add_portfolio_error = "Missing title";
       }
-      else if (!new_portfolio.date || !is_valid_date(new_portfolio.date){
+      else if (!new_portfolio.date){
         $scope.add_portfolio_error = "You must provide a date in format yyyy/mm/dd";
       }
       else if (!new_portfolio.description){
@@ -39,4 +39,4 @@ function(){
       };
     };
   }
-}();
+})();
